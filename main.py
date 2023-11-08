aaa0842b3b105aa0e5955e921ee626ea21f65347
gabriel <gabriel.gregorio1987@gmail.com>
2023-11-08 07:35:20 -0300

    Creating the attributes

--- main.py
+++ main.py
@@ -1,16 +1,8 @@
-# This is a sample Python script.
+name = input("What's your name?")
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+balance = 0
+withdraw_limit = 500
+withdraw_limit_per_day = 3
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
